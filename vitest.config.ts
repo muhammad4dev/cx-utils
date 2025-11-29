9f2aea5b754e3cf62c412ce6adc68a7322d93bed
Muhammad Mustafa <muhammadidev@outlook.com>
2025-11-29 16:25:40 +0000

    test: exclude the barrel file.

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
             provider: 'v8',
             reporter: ['text', 'json', 'html'],
             include: ['src/**/*.ts'],
-            exclude: ['src/**/*.test.ts', 'src/**/*.spec.ts'],
+            exclude: ['src/**/*.test.ts', 'src/**/*.spec.ts', 'src/index.ts'],
             thresholds: {
                 lines: 95,
                 functions: 95,
